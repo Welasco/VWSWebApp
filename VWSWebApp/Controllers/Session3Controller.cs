28cbd17ac7be57b310a84e9509f23e86354fd88a
Victor Santana <victor@hepoca.com>
2018-10-03 11:17:00 -0500

    Adding Session trying to reproduce a issue

--- VWSWebApp/Controllers/Session3Controller.cs
+++ VWSWebApp/Controllers/Session3Controller.cs
@@ -9,21 +9,21 @@ using System.Xml;
 
 namespace VWSWebApp.Controllers
 {
-    public class SessionController : ApiController
+    public class Session3Controller : ApiController
     {
         // GET: api/Session
-        [Route("api/Session")]
+        [Route("api/Session3")]
         public async Task<IHttpActionResult> Get()
         {
             try
             {
-                var endpointUri = new Uri("https://api.github.com/users/welasco");
+                var endpointUri = new Uri("https://websnat2.azurewebsites.net/api/Socket");
 
                 using (HttpClient client = new HttpClient())
                 {
                     //client.DefaultRequestHeaders.Accept.Clear();
                     //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                    
+
 
                     System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
                     client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT; Windows NT 10.0; en-US) WindowsPowerShell/5.1.17134.228");
