033a94115333c5c6c246f994ab25beb2f2c5c7b9
Victor Santana <victor@hepoca.com>
2018-10-04 10:27:32 -0500

    Changing AJAX to be ASYNC

--- VWSWebApp/Controllers/SocketController.cs
+++ VWSWebApp/Controllers/SocketController.cs
@@ -214,7 +214,7 @@ namespace VWSWebApp.Controllers
             }
             catch (Exception e)
             {
-                response = Request.CreateResponse(HttpStatusCode.BadRequest, "Unable to DeserializeObject" + data + "\r\n Exception: " + e.Message);
+                response = Request.CreateResponse(HttpStatusCode.BadRequest, "Exception: " + e.Message);
                 return response;
             }
             return response;
